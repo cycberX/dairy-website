aba8e3b2d926cc39b0d99bf068c5c6a0dce05f3d
Sanskar-cyber19 <bandgarsanskar19@gmail.com>
2025-01-30 23:55:13 +0530

    first commit

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,22 +1,13 @@
-const reportWebVitals = onPerfEntry => {
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
 export default reportWebVitals;
-
-// // src/reportWebVitals.js
-// import { reportWebVitals } from "web-vitals";
-
-// const reportWebVital = (metric) => {
-//   reportWebVitals(metric); // Log the web vitals metrics, you can send them to an analytics service
-// };
-
-// export default reportWebVital;
